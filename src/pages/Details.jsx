3dddecdb1943e0a5a2278d909af4142203ba01b4
soheb21 <neoshoeb53@gmail.com>
2024-02-13 16:46:56 +0530

    Adding Main Layout design

---
+++ src/pages/Details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Details = () => {
+    return (
+        <Layout>
+            <div>Details</div>
+        </Layout>
+    )
+}
+
+export default Details
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+
+
+
+// -left-52
+const Home = () => {
+
+
+    return (
+
+        <Layout>
+            <div className='bg-blue-400 h-full'>Home</div>
+        </Layout>
+
+    )
+}
+
+export default Home
